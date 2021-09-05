18f352e55677ac17a351fd7eeb7f9214d8307efa
marilia camara <mariliacamara@ibm.com>
2021-09-05 13:20:06 -0300

    Moving folders

---
+++ 001-NodeFundamentals/src/index.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const app = express();
+
+app.get("/courses", (req, res) => {
+ return res.json([
+  "Curso 1",
+  "Curso 2",
+  "Curso 3"
+ ]);
+});
+
+app.post("/courses", (req, res) => {
+  return res.json([
+   "Curso 1", 
+   "Curso 2", 
+   "Grupo 3", 
+   "Curso 4"
+  ]);
+});
+
+app.put("/courses/:id", (req, res) => {
+  return res.json([
+   "Curso 6", 
+   "Curso 2", 
+   "Grupo 3", 
+   "Curso 4"
+  ]);
+});
+
+app.patch("/courses/:id", (req, res) => {
+  return res.json([
+   "Curso 6",
+   "Curso 7",
+   "Grupo 3",
+   "Curso 4"
+  ]);
+});
+
+app.delete("/courses/:id", (req, res) => {
+  return res.json([
+   "Curso 6",
+   "Curso 7",
+   "Grupo 3",
+   "Curso 4"
+  ]);
+});
+
+// localhost:3333
+app.listen(3333);
+console.log('Server is running! 🚀');
