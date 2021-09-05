882c07df38206774e5d966be677dcba508fb745e
marilia camara <mariliacamara@ibm.com>
2021-09-05 12:54:17 -0300

    Add nodemon to start server automatically

--- ChapterI/001-NodeFundamentals/src/index.js
+++ ChapterI/001-NodeFundamentals/src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get("/", (req, res) => {
- return res.json({ message: "Hello World, Ignite!"})
+ return res.json({ message: "Hello World, Ignite! - Fundamentos NodeJS"})
 });
 
 // localhost:3333
