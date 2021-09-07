2fdd59fe62807c41d62014c5ffe20797081a3fc0
marilia camara <mariliacamara@ibm.com>
2021-09-07 09:40:48 -0300

    Create methods to register new accounts and validating existing accounts

---
+++ 002-FirstProject/README.md
@@ -0,0 +1,5 @@
+# First Project using NodeJS
+
+In this module, we create our first project with NodeJS from zero, practicing all the concepts we've been studying from the first module.
+
+
---
+++ 002-FirstProject/src/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const { v4: uuidv4 } = require("uuid");
+
+const app = express();
+
+app.use(express.json());
+
+const customers = [];
+
+app.get("/account", (req, res) => {
+  return res.json();
+})
+
+app.post("/account", (req, res) => {
+  const { cpf, name } = req.body;
+
+  const customerAlreadyExists = customers.some(
+    (customer) => customer.cpf === cpf
+  );
+
+  if (customerAlreadyExists) {
+   return res.status(400).json({ error: "Customer already exists!" })
+  }
+
+  customers.push({
+    cpf,
+    name,
+    id: uuidv4(),
+    statment: [],
+  });
+
+  return res.status(201).send();
+});
+
+app.listen(3333);
+console.log('Server is running! 👻')
